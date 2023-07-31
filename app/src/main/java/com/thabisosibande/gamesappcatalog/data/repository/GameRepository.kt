1c8de44417f077fc4e415d05e6e6230dcd73bc2b
Thabiso Sibande <thabisogoodwillsibande@gmail.com>
2023-07-31 18:00:14 +0200

    create intermediary class between the viewmodel and service

--- app/src/main/java/com/thabisosibande/gamesappcatalog/data/repository/GameRepository.kt
+++ app/src/main/java/com/thabisosibande/gamesappcatalog/data/repository/GameRepository.kt
@@ -1,4 +1,7 @@
-package com.thabisosibande.gamesappcatalog
+package com.thabisosibande.gamesappcatalog.data.repository
+
+import com.thabisosibande.gamesappcatalog.GameApiService
+import com.thabisosibande.gamesappcatalog.data.model.Game
 
 class GameRepository(private val gameApiService: GameApiService) {
     suspend fun getGamesForPlatform(platform: String): List<Game> {
