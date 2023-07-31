a5bbe7b2db871551ebec30731fdee3d3d8cd7137
Thabiso Sibande <thabisogoodwillsibande@gmail.com>
2023-07-31 18:01:53 +0200

    create view model to fetch the gamesList data

--- app/src/main/java/com/thabisosibande/gamesappcatalog/ui/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/thabisosibande/gamesappcatalog/ui/viewmodels/GameViewModel.kt
@@ -1,9 +1,12 @@
-package com.thabisosibande.gamesappcatalog
+package com.thabisosibande.gamesappcatalog.ui.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.thabisosibande.gamesappcatalog.GameApiService
+import com.thabisosibande.gamesappcatalog.data.repository.GameRepository
+import com.thabisosibande.gamesappcatalog.data.model.Game
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,12 +19,19 @@ class GameViewModel : ViewModel() {
     private val _games = MutableLiveData<List<Game>>()
     val games: LiveData<List<Game>> get() = _games
 
+    // Variable to hold the total number of games
+    private val _totalGamesCount = MutableLiveData<Int>()
+    val totalGamesCount: LiveData<Int> get() = _totalGamesCount
+
     // Function to fetch the games for the specified platform
     fun getGamesForPlatform(platform: String) {
         viewModelScope.launch {
             try {
                 val gamesList = gameRepository.getGamesForPlatform(platform)
                 _games.postValue(gamesList)
+
+                // Update the total number of games
+                _totalGamesCount.postValue(gamesList.size)
             } catch (e: Exception) {
                 // Handle error if the API request was not successful
                 // e.g., Show an error message to the user
